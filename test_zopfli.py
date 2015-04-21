4bee9a4ab78d3e403d7cd907612b72fd5c579d93
Akinori Hattori <hattya@gmail.com>
2015-04-21 20:02:26 +0900

    Add new class ZopfliDecompressor

--- test_zopfli.py
+++ test_zopfli.py
@@ -16,7 +16,9 @@
 #   limitations under the License.
 #
 
+import sys
 import unittest
+import zlib
 
 import zopfli
 
@@ -27,3 +29,31 @@ class ZopfliTestCase(unittest.TestCase):
         self.assertEqual(zopfli.ZOPFLI_FORMAT_GZIP, 0)
         self.assertEqual(zopfli.ZOPFLI_FORMAT_ZLIB, 1)
         self.assertEqual(zopfli.ZOPFLI_FORMAT_DEFLATE, 2)
+
+    def test_gzip(self):
+        c = zlib.compressobj(9, zlib.DEFLATED, zlib.MAX_WBITS + 16)
+        d = zopfli.ZopfliDecompressor(zopfli.ZOPFLI_FORMAT_GZIP)
+        self._test_zopfli(c, d)
+
+    def test_zlib(self):
+        c = zlib.compressobj(9, zlib.DEFLATED, zlib.MAX_WBITS)
+        d = zopfli.ZopfliDecompressor(zopfli.ZOPFLI_FORMAT_ZLIB)
+        self._test_zopfli(c, d)
+
+    def test_deflate(self):
+        c = zlib.compressobj(9, zlib.DEFLATED, -zlib.MAX_WBITS)
+        d = zopfli.ZopfliDecompressor(zopfli.ZOPFLI_FORMAT_DEFLATE)
+        self._test_zopfli(c, d)
+
+    def _test_zopfli(self, c, d):
+        b = b'Hello, world!'
+        z = c.compress(b) + c.flush()
+        self.assertEqual(d.decompress(z) + d.flush(), b)
+        self.assertEqual(d.unused_data, b'')
+        self.assertEqual(d.unconsumed_tail, b'')
+        if sys.version_info >= (3, 3):
+            self.assertTrue(d.eof)
+
+    def test_unknown(self):
+        with self.assertRaises(ValueError):
+            zopfli.ZopfliDecompressor(-1)
--- zopfli.py
+++ zopfli.py
@@ -18,8 +18,45 @@
 
 """Zopfli Compression Algorithm"""
 
+import sys
+import zlib
+
 from _zopfli import (ZOPFLI_FORMAT_GZIP, ZOPFLI_FORMAT_ZLIB, ZOPFLI_FORMAT_DEFLATE)
 
 
 __version__ = '1.0.dev'
-__all__ = ['ZOPFLI_FORMAT_GZIP', 'ZOPFLI_FORMAT_ZLIB', 'ZOPFLI_FORMAT_DEFLATE']
+__all__ = ['ZOPFLI_FORMAT_GZIP', 'ZOPFLI_FORMAT_ZLIB', 'ZOPFLI_FORMAT_DEFLATE',
+           'ZopfliDecompressor']
+
+
+class ZopfliDecompressor(object):
+
+    def __init__(self, format=ZOPFLI_FORMAT_DEFLATE):
+        if format == ZOPFLI_FORMAT_GZIP:
+            wbits = zlib.MAX_WBITS + 16
+        elif format == ZOPFLI_FORMAT_ZLIB:
+            wbits = zlib.MAX_WBITS
+        elif format == ZOPFLI_FORMAT_DEFLATE:
+            wbits = -zlib.MAX_WBITS
+        else:
+            raise ValueError('unknown format')
+        self.__z = zlib.decompressobj(wbits)
+
+    @property
+    def unused_data(self):
+        return self.__z.unused_data
+
+    @property
+    def unconsumed_tail(self):
+        return self.__z.unconsumed_tail
+
+    if sys.version_info >= (3, 3):
+        @property
+        def eof(self):
+            return self.__z.eof
+
+    def decompress(self, *args, **kwargs):
+        return self.__z.decompress(*args, **kwargs)
+
+    def flush(self, *args, **kwargs):
+        return self.__z.flush(*args, **kwargs)
