a7ce32096b77498a9b5d7bc9afc39ec887307ae0
Akinori Hattori <hattya@gmail.com>
2020-11-08 22:30:13 +0900

    Update version → dev

--- zopfli.py
+++ zopfli.py
@@ -19,7 +19,7 @@ from _zopfli import (ZOPFLI_FORMAT_GZIP, ZOPFLI_FORMAT_ZLIB, ZOPFLI_FORMAT_DEFLA
                      ZopfliCompressor, ZopfliDeflater, ZopfliPNG)
 
 
-__version__ = '1.4'
+__version__ = '1.5.dev'
 __all__ = ['ZOPFLI_FORMAT_GZIP', 'ZOPFLI_FORMAT_ZLIB', 'ZOPFLI_FORMAT_DEFLATE',
            'ZopfliCompressor', 'ZopfliDeflater', 'ZopfliDecompressor', 'ZopfliPNG',
            'ZipFile', 'ZipInfo']
