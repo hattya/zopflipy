4303c87dc5fd3658f4f58449e6682124b2e4e517
Akinori Hattori <hattya@gmail.com>
2024-03-24 21:26:40 +0900

    setup: drop test command

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@
 #
 
 import os
-import sys
 
-from setuptools import setup, Command, Extension
+from setuptools import setup, Extension
 
 
 def sources(path, exts):
@@ -19,39 +18,8 @@ def sources(path, exts):
                 yield os.path.normpath(os.path.join(root, f))
 
 
-class test(Command):
-
-    description = 'run unit tests'
-    user_options = [('failfast', 'f', 'stop on first fail or error')]
-
-    boolean_options = ['failfast']
-
-    def initialize_options(self):
-        self.failfast = False
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        import unittest
-
-        build_ext = self.reinitialize_command('build_ext')
-        build_ext.inplace = True
-        self.run_command('build_ext')
-        # run unittest discover
-        argv = [sys.argv[0], 'discover', '--start-directory', 'tests']
-        if self.verbose:
-            argv.append('--verbose')
-        if self.failfast:
-            argv.append('--failfast')
-        unittest.main(None, argv=argv)
-
-
 setup(
     ext_modules=[Extension('zopfli._zopfli',
                            include_dirs=[os.path.join('zopfli', '_zopfli', 'zopfli', 'src')],
                            sources=list(sources('zopfli', ['.c', '.cc', '.cpp'])))],
-    cmdclass={
-        'test': test,
-    },
 )
